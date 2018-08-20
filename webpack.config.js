e2b70a5af0c305eb78926b4e4db03474fadaae69
wz333333 <32127273+wz333333@users.noreply.github.com>
2018-08-20 08:40:45 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,44 +6,30 @@ const webpack = require('webpack');
 
 module.exports = {
     entry:{
-        "javascripts/app": './public/javascripts/index.js',
-        // print: './src/print.js'
+        "javascripts/app": './public/javascripts/index.js'
     },
     output:{
         filename: '[name].[chunkhash].js',
         path:path.resolve(__dirname,"dist"),
         publicPath: '/'
     },
-    // resolve:{
-    //     alias:{
-    //         'imanges': path.resolve(__dirname, '../public/images')
-    //     }
-    // },
     devtool: 'eval-source-map',
     plugins:[
         new CleanWebpackPlugin(['dist']),
         new VueLoaderPlugin(),
         new HtmlWebpackPlugin({
             filename: 'index.html',
-            template:'public/index.html',
-            // chunks:["javascripts/app","javascripts/common.chunk"]
-        }),
-        // new CommonsChunkPlugin({
-        //     name: "javascripts/common.chunk",
-        //     chunks: ["javascripts/ui", "javascripts/router"]
-        // })
-        // new webpack.NamedModulesPlugin()
+            template:'public/index.html'
+        })
     ],
     module: {
         rules: [
 			{test: /\.vue$/, loader: 'vue-loader'},
 			{test: /\.css$/, use: ['style-loader','css-loader']},
-            {test: /\.js$/, loader: 'babel-loader'},
-            // {test : /\.json$/,loader : 'json-loader'},在webpack2.0以上版本不需要再使用json-loader模块
+            {test: /\.js$/, loader: 'babel-loader'}
             {test: /\.(eot|svg|ttf|woff|woff2)$/,loader: 'file-loader'},
             {test: /\.html$/,loader: 'html-withimg-loader'},
-            {test:/\.(jpg|png|gif|jpeg)$/,loader: 'file-loader'},
-            // {test: /\.(jpg|png|jpeg|gif)$/,loader: 'url-loader?name=images/[hash:8].[name].[ext]'}
+            {test:/\.(jpg|png|gif|jpeg)$/,loader: 'file-loader'}
         ]
     },
     
